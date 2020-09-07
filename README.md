40012167be5153afac7b0c424f2983918547ebc1
Vadim Kantorov <vadimkantorov@gmail.com>
2020-09-07 18:11:41 +0200

    ...

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-### [WIP] Build a minimal XeTeX from source in TexLive 2020
+# [WIP] Build a minimal XeTeX from source in TexLive 2020
 
 ```shell
+
+# clean
+bash clean.sh
+
 # build native tools
 bash build_native.sh
 
 # build wasm tools
 bash build_wasm.sh
+```
+
+### Versions
+```shell
+emsdk install 2.0.0 # 5974288502aab433d45f53511e961aaca4079d86
 ```
--- build_wasm.sh
+++ build_wasm.sh
@@ -1,9 +1,6 @@
 export MAKEFLAGS=-j4
 
 SUFFIX=wasm
-EMMAKE=emmake
-EMCMAKE=emcmake
-EMCONFIGURE=emconfigure
 
 ROOT=$PWD
 
@@ -22,13 +19,23 @@ TEXLIVE_SOURCE_URL=https://github.com/TeX-Live/texlive-source/archive/9ed922e7d2
 TEXLIVE_SOURCE_NAME=texlive-source-$(basename $TEXLIVE_SOURCE_URL .tar.gz)
 TEXLIVE_SOURCE_DIR=$ROOT/$TEXLIVE_SOURCE_NAME
 TEXLIVE_BUILD_DIR=$TEXLIVE_SOURCE_DIR/texlive-build-$SUFFIX
+WEB2C_NATIVE_TOOLS_DIR=$TEXLIVE_SOURCE_DIR/texlive-build-native/texk/web2c
 
 BACKUP=$ROOT/texlive-backup
 PREFIX=$ROOT/prefix-$SUFFIX
-CACHE=$ROOT/config-$SUFFIX.cache
+TEXLIVE_CACHE=$ROOT/config-$SUFFIX.cache
 XELATEX_EXE=$PREFIX/bin/xelatex
 XETEX_EXE=$PREFIX/bin/xetex
-#export EM_PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+
+
+EMMAKE=emmake
+EMCMAKE=emcmake
+EMCONFIGURE=emconfigure
+EMCCSKIP_FREETYPE="python3 $ROOT/ccskip.py $TEXLIVE_SOURCE_DIR/texlive-build-native/libs/freetype2/ft-build/apinames --"
+EMCCSKIP_XETEX="python3 $ROOT/ccskip.py $WEB2C_NATIVE_TOOLS_DIR/ctangle $WEB2C_NATIVE_TOOLS_DIR/ctangleboot $WEB2C_NATIVE_TOOLS_DIR/web2c/fixwrites $WEB2C_NATIVE_TOOLS_DIR/web2c/splitup $WEB2C_NATIVE_TOOLS_DIR/tangle $WEB2C_NATIVE_TOOLS_DIR/tangleboot $WEB2C_NATIVE_TOOLS_DIR/tie $WEB2C_NATIVE_TOOLS_DIR/web2c/web2c $WEB2C_NATIVE_TOOLS_DIR/otangle $WEB2C_NATIVE_TOOLS_DIR/web2c/makecpool $WEB2C_NATIVE_TOOLS_DIR/xetex --"
+EMCCFLAGS_FONTCONFIG="-Duuid_generate_random=uuid_generate"
+EMCCFLAGS_TEXLIVE="-s ERROR_ON_UNDEFINED_SYMBOLS=0 -DELIDE_CODE"
+
 
 mkdir -p $PREFIX $PREFIX/bin
 mkdir -p $BACKUP/texk $BACKUP/texk/web2c
@@ -42,13 +49,12 @@ cd $TEXLIVE_SOURCE_DIR
 mkdir -p texlive-build-$SUFFIX
 cd texlive-build-$SUFFIX
 
-echo 'ac_cv_func_getwd=${ac_cv_func_getwd=no}' > $CACHE
+echo 'ac_cv_func_getwd=${ac_cv_func_getwd=no}' > $TEXLIVE_CACHE
 echo > $FONTCONFIG_CACHE
 
-EMCCFLAGS="-s ERROR_ON_UNDEFINED_SYMBOLS=0 -DELIDE_CODE"
-CFLAGS="$EMCCFLAGS -I$PREFIX/include -I$TEXLIVE_SOURCE_DIR/texlive-build-$SUFFIX/libs/icu/include -I$FONTCONFIG_SOURCE_DIR"
-$EMCONFIGURE ../configure                                    \
-  --cache-file=$CACHE                           \
+CFLAGS="$EMCCFLAGS_TEXLIVE -I$PREFIX/include -I$TEXLIVE_SOURCE_DIR/texlive-build-$SUFFIX/libs/icu/include -I$FONTCONFIG_SOURCE_DIR"
+$EMCONFIGURE ../configure                       \
+  --cache-file=$TEXLIVE_CACHE                   \
   --prefix=$PREFIX                              \
   --enable-dump-share                           \
   --enable-static                               \
@@ -76,9 +82,10 @@ $EMCONFIGURE ../configure                                    \
   --without-system-freetype2                    \
   --without-system-libpng                       \
   --without-system-zlib                         \
-  --with-fontconfig-includes="$ROOT/$FONTCONFIG_SOURCE_NAME"                         \
-  --with-fontconfig-libdir="$ROOT/$FONTCONFIG_SOURCE_NAME/build-$SUFFIX/src/.libs"   \
-  --with-banner-add="_BLFS" CFLAGS="$CFLAGS" CPPFLAGS="$CFLAGS"
+  --with-banner-add="_EM" CFLAGS="$CFLAGS" CPPFLAGS="$CFLAGS"
+
+#  --with-fontconfig-includes="$ROOT/$FONTCONFIG_SOURCE_NAME"                         \
+#  --with-fontconfig-libdir="$ROOT/$FONTCONFIG_SOURCE_NAME/build-$SUFFIX/src/.libs"   \
 
 $EMMAKE make $MAKEFLAGS 
 $EMMAKE make $MAKEFLAGS install
@@ -90,8 +97,7 @@ for d in texk/dvipdfm-x libs/teckit libs/harfbuzz libs/graphite2 libs/libpng lib
 done
 
 pushd libs/freetype2
-CC="python3 $ROOT/ccskip.py $TEXLIVE_SOURCE_DIR/texlive-build-native/libs/freetype2/ft-build/apinames --"
-$EMMAKE make $MAKEFLAGS CC="$CC emcc"
+$EMMAKE make $MAKEFLAGS CC="$EMCCSKIP_FREETYPE emcc"
 popd
 
 pushd libs/icu/icu-build
@@ -133,20 +139,21 @@ patch -d .. -Np1 -i $ROOT/0002-fix-fcstats-emscripten.patch
 echo 'all install:' > ../test/Makefile.in
 FREETYPE_CFLAGS="-I$TEXLIVE_BUILD_DIR/libs/freetype2/ -I$TEXLIVE_BUILD_DIR/libs/freetype2/freetype2"
 FREETYPE_LIBS="-L$TEXLIVE_BUILD_DIR/libs/freetype2/ -lfreetype"
-EM_PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig $EMCONFIGURE ../configure \
+$EMCONFIGURE ../configure \
     --cache-file $FONTCONFIG_CACHE \
     --prefix=$PREFIX \
     --enable-static \
     --disable-shared \
-    --disable-docs CFLAGS="-Duuid_generate_random=uuid_generate" \
-    --with-expat-includes="$EXPAT_SOURCE_DIR/lib" --with-expat-lib="$EXPAT_BUILD_DIR" FREETYPE_CFLAGS="$FREETYPE_CFLAGS" FREETYPE_LIBS="$FREETYPE_LIBS" 
+    --disable-docs CFLAGS="$EMCCFLAGS_FONTCONFIG" \
+    --with-expat-includes="$EXPAT_SOURCE_DIR/lib" \
+    --with-expat-lib="$EXPAT_BUILD_DIR" \
+    FREETYPE_CFLAGS="$FREETYPE_CFLAGS" FREETYPE_LIBS="$FREETYPE_LIBS" 
 $EMMAKE make $MAKEFLAGS 
 
 cd $TEXLIVE_SOURCE_DIR/texlive-build-$SUFFIX/texk/web2c
-WEB2C_TOOLS_DIR=$TEXLIVE_SOURCE_DIR/texlive-build-native/texk/web2c
-CC="python3 $ROOT/ccskip.py $WEB2C_TOOLS_DIR/ctangle $WEB2C_TOOLS_DIR/ctangleboot $WEB2C_TOOLS_DIR/web2c/fixwrites $WEB2C_TOOLS_DIR/web2c/splitup $WEB2C_TOOLS_DIR/tangle $WEB2C_TOOLS_DIR/tangleboot $WEB2C_TOOLS_DIR/tie $WEB2C_TOOLS_DIR/web2c/web2c $WEB2C_TOOLS_DIR/otangle $WEB2C_TOOLS_DIR/web2c/makecpool $WEB2C_TOOLS_DIR/xetex --"
-$EMMAKE make $MAKEFLAGS xetex CC="$CC emcc" CXX="$CC em++"
+$EMMAKE make $MAKEFLAGS xetex CC="$EMCCSKIP_XETEX emcc" CXX="$EMCCSKIP em++"
 
-em++ -g -O2 -o xetex xetexdir/xetex-xetexextra.o synctexdir/xetex-synctex.o xetex-xetexini.o xetex-xetex0.o xetex-xetex-pool.o  libxetex.a $TEXLIVE_BUILD_DIR/libs/harfbuzz/libharfbuzz.a $TEXLIVE_BUILD_DIR/libs/graphite2/libgraphite2.a $TEXLIVE_BUILD_DIR/libs/teckit/libTECkit.a $TEXLIVE_BUILD_DIR/libs/libpng/libpng.a $TEXLIVE_BUILD_DIR/libs/freetype2/libfreetype.a $TEXLIVE_BUILD_DIR/libs/pplib/libpplib.a $TEXLIVE_BUILD_DIR/libs/zlib/libz.a libmd5.a lib/lib.a $TEXLIVE_BUILD_DIR/texk/kpathsea/.libs/libkpathsea.a $FONTCONFIG_BUILD_DIR/src/.libs/libfontconfig.a $EXPAT_BUILD_DIR/libexpat.a $TEXLIVE_BUILD_DIR/libs/icu/icu-build/lib/libicuuc.a -s ERROR_ON_UNDEFINED_SYMBOLS=0
+XETEX_OBJECTS="xetexdir/xetex-xetexextra.o synctexdir/xetex-synctex.o xetex-xetexini.o xetex-xetex0.o xetex-xetex-pool.o libxetex.a"
+XETEX_DEPS="$TEXLIVE_BUILD_DIR/libs/harfbuzz/libharfbuzz.a $TEXLIVE_BUILD_DIR/libs/graphite2/libgraphite2.a $TEXLIVE_BUILD_DIR/libs/teckit/libTECkit.a $TEXLIVE_BUILD_DIR/libs/libpng/libpng.a $TEXLIVE_BUILD_DIR/libs/freetype2/libfreetype.a $TEXLIVE_BUILD_DIR/libs/pplib/libpplib.a $TEXLIVE_BUILD_DIR/libs/zlib/libz.a libmd5.a lib/lib.a $TEXLIVE_BUILD_DIR/texk/kpathsea/.libs/libkpathsea.a $FONTCONFIG_BUILD_DIR/src/.libs/libfontconfig.a $EXPAT_BUILD_DIR/libexpat.a $TEXLIVE_BUILD_DIR/libs/icu/icu-build/lib/libicuuc.a"
 
-#$PREFIX/lib/libfontconfig.a
+em++ -g -O2 -o xetex $XETEX_OBJECTS $XETEX_DEPS -s ERROR_ON_UNDEFINED_SYMBOLS=0
